62948588a202bcb6953a2e5aaf8a17a74a5520aa
Vittorio Accomazzi <Vittorio.Accomazzi@gmail.com>
2020-07-23 20:31:49 -0400

    re #28 -- fixed map link

--- src/components/AppHelp.js
+++ src/components/AppHelp.js
@@ -22,7 +22,7 @@ export default function  AppHelp ({handleClose, open}) {
             <DialogContent>
               <DialogContentText id="alert-dialog-description">
                 Welcome to the Accomazzo Documents Repository. The Repository is composed of more then 2,000 documents gathered 
-                from <a href="https://www.google.com/maps/@44.9904498,8.2749082,3a,75y,51.47h,98.88t/data=!3m6!1e1!3m4!1sG8XUjhgyk2B9y02kbOn7iw!2e0!7i13312!8i6656"> San Desiderio Church</a> in
+                from <a href="https://www.google.com/maps/@44.9904498,8.2749082,3a,75y,51.47h,98.88t/data=!3m6!1e1!3m4!1sG8XUjhgyk2B9y02kbOn7iw!2e0!7i13312!8i6656" target="_blank"> San Desiderio Church</a> in
                 Italy and also from the Calliano municipality.<br/>
                 The repository contains Birth certificates, Marriage certificates and few death certificates. There are documents in italian (about 50%), latin (about 40%) and french (about 10%).
                 Therefore, when searching for a particular  individual make sure to try different spelling.
